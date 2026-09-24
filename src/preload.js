6dea0838ae12f123783bbe891b3172a1fd99e0ab
BaekJongHyeok <81210049+BaekJongHyeok@users.noreply.github.com>
2026-09-24 12:01:08 +0900

    v0.3: expose subscription automation IPC

--- src/preload.js
+++ src/preload.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('aiOffice', {
   openSubscription: (provider, prompt = '') => ipcRenderer.invoke('open-subscription', { provider, prompt }),
+  automateSubscription: (provider, prompt = '') => ipcRenderer.invoke('automate-subscription', { provider, prompt }),
   copyText: (text) => ipcRenderer.invoke('copy-text', text),
   getSessionInfo: (provider) => ipcRenderer.invoke('session-info', provider),
   clearSubscriptionSession: (provider) => ipcRenderer.invoke('clear-subscription-session', provider),
